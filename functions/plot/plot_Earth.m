1fe6dcfcd77af2105400f7e4dc960cbfd65f52ff
AlbeChio <115644182+AlbeChio@users.noreply.github.com>
2022-10-12 16:38:00 +0200

    plot_earth
    plot the 3d orbit

---
+++ functions/plot/plot_Earth.m
@@ -0,0 +1,22 @@
+function [] = plot_Earth(r)
+% [] = plot_Earth(r)
+% version: 1.1
+%
+% author: Albe
+% input:    
+% r         position state vector
+% output:
+%
+
+figure
+plot3(r(:,1),r(:,2),r(:,3),'Color','y')
+hold on
+opts.Units='km';         %this command i needed to set the units of the planet3D, otherwise the Earth is enormous and covers all the other plots
+h=planet3D('Earth',opts);%this function is better than earth_sphere since the colorbar doesn't go crazy. Try to use earth_sphere to verify it.
+h.Annotation.LegendInformation.IconDisplayStyle = 'off'; %it prevent the planet3D function to print useless information on the figure's legend.
+grid on
+legend('Orbit','Location','Best');
+
+
+end
+
